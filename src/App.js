a6449ff14971e10e5b357f4b259b425f98af03c4
Nguyen Nguyen <n.dinhnguyen95@gmail.com>
2020-03-26 01:03:00 +0700

    added FB login + posted score

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Board from './components/Board';
+import FacebookLogin from 'react-facebook-login';
 
+const APP_ID = process.env.REACT_APP_APP_ID;
+let startTime = 0;
+let endTime = 0;
+let TIME_ELAPSED_IN_SECONDS;
 class App extends Component {
   constructor(props){
     super(props)
@@ -10,10 +15,14 @@ class App extends Component {
       history: [{
         squares: ['','','','','','','','','']
       }],
+      leaderBoard: [],
       stepNumber: 0,
+      user: '',
       xIsNext: false, //false is X, true is O
     }
   }
+
+  
 
   calculateWinner = (squares) => {
     const lines = [
@@ -37,6 +46,10 @@ class App extends Component {
   onSquareClicked = (i) => {
     const history = this.state.history.slice(0, this.state.stepNumber+1);
     console.log('history:', history);
+    if(this.state.stepNumber===0){
+      startTime=Date.now();
+      console.log('startTime',startTime);
+    }
     const current = history[history.length - 1];
     console.log('current:', current);
     const squareList = current.squares.slice();
@@ -59,7 +72,38 @@ class App extends Component {
     this.setState({
       stepNumber: step,
       xIsNext: (step%2)===0
-    })
+    });
+  }
+
+  responseFacebook = (response) => {
+    console.log("result from facebook:",response);
+    this.setState({user: response.name}); //when we get response from FB, 
+  }
+
+  postData =  async (timeScore) => {
+    let data = new URLSearchParams();
+
+    data.append("player", this.state.user); //data you want to post (key, value)
+    data.append("score", timeScore);
+
+    const url = `https://ftw-highscores.herokuapp.com/tictactoe-dev`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: data.toString(),
+      json: true
+    });
+    this.getData();
+  }
+
+  getData = async () => {
+    const url = `https://ftw-highscores.herokuapp.com/tictactoe-dev`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data from API",data);
+    // this.setState({loaderBoard: data.items})
   }
 
   render(){
@@ -79,21 +123,40 @@ class App extends Component {
 
     let status = '';  
 
+    let timeScore;
+ 
     if(winner){
+        timeScore = Math.floor((Date.now()-startTime)/1000);
+        this.postData(timeScore);
         status = `Winner is ${winner}!`;
         console.log('Winner is:', winner);
     } else {
         status = this.state.xIsNext?`NextPlayer is O`:`NextPlayer is X`
     }
 
+    if(!this.state.user){
+      return (
+        <FacebookLogin
+        appId={APP_ID}
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={this.responseFacebook} />
+      )
+    }
     return (
-      <div>
+      <div>     
         <h1>Tic Tac Toe</h1>
+        <h2>User info: {this.state.user}</h2>
         <h2>{status}</h2>
         <div className="game-area d-flex justify-content-between">
-        <Board {...this.state} status={status} squareList={current.squares} onClick={(i)=>this.onSquareClicked(i)} calculateWinner={this.calculateWinner}/>
+        <Board {...this.state} status={status} squareList={current.squares} 
+        postData={this.postData()}
+        onClick={(i)=>this.onSquareClicked(i)} calculateWinner={this.calculateWinner}/>
         <div className="game-info">
             <ol>{moves}</ol>
+            <div>Score: {timeScore}</div>
+            {/* <div>{this.state.leaderBoard.player}</div>
+            <div>{this.state.leaderBoard.score}</div> */}
         </div>
         </div>
         {/* <Board squares={this.state.squares} nextPlayer={this.state.nextPlayer} setParentsState={this.setParentsState}/> */}
